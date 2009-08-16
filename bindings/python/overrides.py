32429d0f437516504634e7d6e457ed571bdd90ad
Lincoln de Sousa <lincoln@minaslivre.org>
2009-08-16 01:33:34 -0300

    Adding new overrides to atom content and atom entry.
    The atom_content_new and atom_entry_add_author functions were
    overriden to bypass some C ish problems, like the need of passing a
    string length. The python object standard name changed in last
    commit was updated in already added overrides.

--- bindings/python/overrides.py
+++ bindings/python/overrides.py
@@ -17,11 +17,29 @@
 # Free Software Foundation, Inc., 59 Temple Place - Suite 330,
 # Boston, MA 02111-1307, USA.
 
+def j_atom_content_new():
+    return '''
+static int
+AtomContent_init (PyAtomContentObject *self,
+                  PyObject            *args,
+                  PyObject            *kwargs)
+{
+  const char * _type;
+  const char * _content;
+  int _len;
+  if (!PyArg_ParseTuple (args, "ss", &_type, &_content))
+    return -1;
+  _len = strlen (_content);
+  self->inner = j_atom_content_new (_type, _content, _len);
+  return 0;
+}
+'''
+
 def j_atom_entry_set_updated():
     return '''
 static PyObject *
-AtomEntry_set_updated (AtomEntry *self,
-                       PyObject  *args)
+AtomEntry_set_updated (PyAtomEntryObject *self,
+                       PyObject          *args)
 {
   PyObject *date;
   time_t cdate;
@@ -55,8 +73,8 @@ AtomEntry_set_updated (AtomEntry *self,
 def j_atom_entry_get_updated():
     return '''
 static PyObject *
-AtomEntry_get_updated (AtomEntry *self,
-                       PyObject  *args)
+AtomEntry_get_updated (PyAtomEntryObject *self,
+                       PyObject          *args)
 {
   PyObject *date;
   time_t cdate;
@@ -73,7 +91,24 @@ AtomEntry_get_updated (AtomEntry *self,
 }
 '''
 
+def j_atom_entry_add_author():
+    return '''
+static PyObject *
+AtomEntry_add_author (PyAtomEntryObject *self,
+                      PyObject          *args)
+{
+  PyAtomAuthorObject * _author;
+  if (!PyArg_ParseTuple (args, "O", &_author))
+    return NULL;
+  Py_INCREF (_author);
+  j_atom_entry_add_author (self->inner, _author->inner);
+  return Py_BuildValue ("");
+}
+'''
+
 OVERRIDES = {
+    'j_atom_content_new': j_atom_content_new,
     'j_atom_entry_set_updated': j_atom_entry_set_updated,
     'j_atom_entry_get_updated': j_atom_entry_get_updated,
+    'j_atom_entry_add_author': j_atom_entry_add_author,
 }
